6353be178b8e522d94d84c9a5bc305e7cd8df116
Marco Ferri <m.ferri17@campus.unimib.it>
2017-12-25 18:30:08 +0100

    creato lo scheletro del progetto

--- BrewDay/App_Start/BundleConfig.cs
+++ BrewDay/App_Start/BundleConfig.cs
@@ -8,24 +8,22 @@ namespace BrewDay
         // Per altre informazioni sulla creazione di bundle, vedere https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/_wwwroot/vendor/jquery-ui/jquery-ui.css",
+                "~/_wwwroot/vendor/bootstrap/css/bootstrap.css",
+                "~/_wwwroot/vendor/datatables/datatables.css",
+                "~/_wwwroot/css/site.css"));
 
-            // Utilizzare la versione di sviluppo di Modernizr per eseguire attività di sviluppo e formazione. Successivamente, quando si è
-            // pronti per passare alla produzione, usare lo strumento di compilazione disponibile all'indirizzo https://modernizr.com per selezionare solo i test necessari.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/_wwwroot/vendor/jquery/jquery-3.2.1.js",
+                "~/_wwwroot/vendor/jquery-ui/jquery-ui.js",
+                "~/_wwwroot/vendor/bootstrap/js/bootstrap.js",
+                "~/_wwwroot/vendor/respond/respond.js",
+                "~/_wwwroot/vendor/datatables/datatables.js",
+                "~/_wwwroot/js/site.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
         }
     }
 }
