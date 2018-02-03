79276a4d76a48f772c984a76d733670a7f4bb2b6
Paolo Marconi <p.marconi2@campus.unimib.it>
2018-02-03 15:03:50 +0100

    Aggiunto DP Singleton e fixato bug sulla modifica ricetta

--- BrewDay.Domain/BrewDayContext.cs
+++ BrewDay.Domain/BrewDayContext.cs
@@ -8,18 +8,45 @@ namespace BrewDay.Domain
 {
     public class BrewDayContext : DbContext
     {
+        private static BrewDayContext BrewDayContext_instance = null;
         // You can add custom code to this file. Changes will not be overwritten.
         // 
         // If you want Entity Framework to drop and regenerate your database
         // automatically whenever you change your model schema, please use data migrations.
         // For more information refer to the documentation:
         // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-    
-        public BrewDayContext() : base("name=BrewDayContext")
+
+        /*public BrewDayContext() : base("name=BrewDayContext")
         {
+
+        }*/
+
+        /* public BrewDayContext()
+         {
+
+             if (BrewDayContext_instance == null)
+                 BrewDayContext_instance = new BrewDayContext();
+
+             return BrewDayContext_instance;
+         }*/
+
+        public static BrewDayContext Instance
+        {
+            get
+            {
+                if (BrewDayContext_instance == null)
+                {                    
+                        if (BrewDayContext_instance == null)
+                        BrewDayContext_instance = new BrewDayContext();
+                   
+                }
+                return BrewDayContext_instance;
+            }
         }
 
-        public DbSet<Recipe> Recipes { get; set; }
+
+
+    public DbSet<Recipe> Recipes { get; set; }
         public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<RecipeIngredient> RecipeIngredients { get; set; }
         public DbSet<Instrument> Instruments { get; set; }
